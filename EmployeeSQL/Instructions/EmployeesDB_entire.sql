a1ef7f2c7cebd0e19c5783c5d402835899922ea7
Bridgette <53711858+bshub6@users.noreply.github.com>
2019-10-27 14:47:06 -0400

    made corrections

--- EmployeeSQL/Instructions/EmployeesDB_entire.sql
+++ EmployeeSQL/Instructions/EmployeesDB_entire.sql
@@ -119,10 +119,10 @@ AND last_name LIKE 'B%';
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
 JOIN employees
-ON dept_.emp_no = employees.emp_no
+ON dept_emp.emp_no = employees.emp_no
 JOIN departments
 ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_name = "Sales";
+WHERE departments.dept_name = 'Sales';
 
 --7
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
